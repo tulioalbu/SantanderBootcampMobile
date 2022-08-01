d675b161ef356ba13f7b75c35a1e1f2e48ba1b7d
tulioalbu <tulioalbu@gmail.com>
2022-07-31 23:33:38 -0300

    Criação do Layout e codificação da Main Activity do App Github Stage

---
+++ githubStage/app/src/main/java/me/dio/githubstage/App.kt
@@ -0,0 +1,16 @@
+package me.dio.githubstage
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application () {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+        }
+    }
+}
